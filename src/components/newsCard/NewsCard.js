8330362688220cca310f2898d90f7105c4dde150
Sarthak Niranjan <22b3923@iitb.ac.in>
2023-11-29 18:17:50 +0530

    fetch api data; populate homepage with data

--- src/components/newsCard/NewsCard.js
+++ src/components/newsCard/NewsCard.js
@@ -2,20 +2,21 @@ import React from "react";
 import "./styles.scss";
 
 const NewsCard = (props) => {
-    let {title, description, imageUrl} = props;
+  //title, desc and image wont change after loading, hence we don't set them as states
+  let { title, description, imageUrl, newsUrl } = props;
   return (
     <div className="card" style={{ width: "18rem" }}>
-      <img
-        src={imageUrl}
-        className="card-img-top"
-        alt="..."
-      />
+      <img src={imageUrl} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
-        <p className="card-text">
-          {description}
-        </p>
-        <a href="/" className="btn btn-primary">
+        <p className="card-text">{description}</p>
+        <a
+          href={newsUrl}
+          rel="noreferrer"
+          //taregt - blank opens the link in a new tab
+          target="_blank"
+          className="btn btn-primary"
+        >
           Read More
         </a>
       </div>
